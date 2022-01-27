bcd525ba93bcc03f1ae48184b0ca5845a3ad355b
Danny Xiong <dx.cruiser07@gmail.com>
2022-01-26 22:27:41 -0500

    template

---
+++ ProjectTemplate/FileController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectTemplate
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("test");
+        }
+    }
+}
